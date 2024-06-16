73ee0ecd82d313a1c2d8691e43161ec4c665e6b0
Paula <eipvi@proton.me>
2024-06-16 15:13:41 -0300

    feat: create ajust api

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use(cors()); // Permite requisições de qualquer origem
 //criar omiddleware para receber os dados no corpo da requisição
 app.use(express.json());
 
-
 //get-É usado para buscar dados do servidor.
 //Não modifica ou envia dados do cliente para o servidor.
 app.get("/", (req, res) => {
@@ -23,6 +22,7 @@ const messages = require("./controllers/messages");
 //criar as rotas
 app.use('/message', messages);
 
-app.listen(3000, () => {
-    console.log("servidor iniciado na porta 3000: http://localhost:3000");
+const port = process.env.PORT || 3001;
+app.listen(3001, () => {
+    console.log("servidor iniciado na porta 3001: http://localhost:3001");
 });
